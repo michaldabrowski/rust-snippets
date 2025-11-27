b7272eeb01acfb051904daf8f12d237c53797b71
Michał Dąbrowski <michal@dabrowski.io>
2025-11-27 20:31:39 +0100

    Programming Rust - Chapter 4 Moves continuation

--- programming-rust/ownership-and-moves/src/main.rs
+++ programming-rust/ownership-and-moves/src/main.rs
@@ -27,4 +27,27 @@ fn main() {
     assert_eq!(third, "103");
 
     assert_eq!(v, vec!["101", "104", "substitute"]);
+
+    let string1 = "someString1".to_string();
+    let string2 = string1;
+    // println!("string1={}", string1); won't compile because string1 has been moved to string2
+    println!("string2={}", string2);
+
+    let num1: i32 = 36;
+    let num2 = num1;
+    println!("num1={}, num2={}", num1, num2); // works because i32 implements the Copy trait
+
+    let l = Label { number: 21 };
+
+    print(l);
+    println!("Label number {}", l.number);
+}
+
+fn print(l: Label) {
+    println!("Label number {}", l.number);
+}
+
+#[derive(Copy, Clone)]
+struct Label {
+    number: u32,
 }
