a0efa107fb9fbceaa30061be8d7fc36d52b43830
Michał Dąbrowski <michal@dabrowski.io>
2025-12-02 22:48:27 +0100

    AoC 2025 - Day 02

--- aoc/2025/day02/src/main.rs
+++ aoc/2025/day02/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::read_to_string;
 
 fn main() {
     let mut invalid_ids_sum: u64 = 0;
+    let mut invalid_ids_sum_2: u64 = 0;
 
     let input = read_to_string("input.txt").expect("Failed to read input file");
 
@@ -18,10 +19,14 @@ fn main() {
             if is_invalid_id(id) {
                 invalid_ids_sum += id;
             }
+            if is_invalid_id_2(id) {
+                invalid_ids_sum_2 += id;
+            }
         }
     }
 
-    println!("{}", invalid_ids_sum)
+    println!("{}", invalid_ids_sum);
+    println!("{}", invalid_ids_sum_2);
 }
 
 fn is_invalid_id(id: u64) -> bool {
@@ -34,4 +39,25 @@ fn is_invalid_id(id: u64) -> bool {
 
     let half = len / 2;
     s[..half] == s[half..]
+}
+
+fn is_invalid_id_2(id: u64) -> bool {
+    let s = id.to_string();
+    let len = s.len();
+
+    for pattern_len in 1..=len / 2 {
+        if len % pattern_len != 0 {
+            continue;
+        }
+
+        let pattern = &s[..pattern_len];
+        if s.chars()
+            .collect::<Vec<_>>()
+            .chunks(pattern_len)
+            .all(|chunk| chunk.iter().collect::<String>() == pattern)
+        {
+            return true;
+        }
+    }
+    false
 }
